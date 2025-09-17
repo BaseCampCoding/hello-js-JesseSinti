b50a3511086a6f33cf6cb1f68f09d207ce6c6086
JesseSinti <jessecorbin8@icloud.com>
2025-09-17 13:54:06 -0500

    finished assignment 3

--- 3-Strings/jasmine/src/main.js
+++ 3-Strings/jasmine/src/main.js
@@ -1,6 +1,9 @@
 let line = "Stands so high Huge hooves too Impatiently waits for Reins and harness Eager to leave" ;
 let frontDoorLetter = line[0];
-let frontDoorPassword = frontDoorLetter + (line[15]).toLowerCase() + (line[31]).toLowerCase() + (line[53]).toLowerCase() + (line[71]).toLowerCase();
+let frontDoorWord = "SHIRE"
+let frontDoorPassword = frontDoorWord[0] + frontDoorWord.slice(1,5).toLowerCase();
 let lineWithWhitespace = "Reins and harness";
-let backDoorLetter = (lineWithWhitespace[16]); 
-let backDoorPassword = `Hor${backDoorLetter}e, `+`please`;
+let removeWhiteSpace = lineWithWhitespace.trim()
+let backDoorLetter = (removeWhiteSpace[removeWhiteSpace.length - 1]); 
+let backDoorWord = "Horse"
+let backDoorPassword = backDoorWord +`, please`;
